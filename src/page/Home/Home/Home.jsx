c8c1028f8ccd7432356c134c516fc231e92401cf
niloysaha06 <niloysaha895@gmail.com>
2023-08-30 02:51:25 +0600

    get buses data & show ui

--- src/page/Home/Home/Home.jsx
+++ src/page/Home/Home/Home.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import BusList from "../BusList/BusList";
+import axios from "axios";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [busesData, setBusesData] = useState([]);
+
+  const getBusData = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:3000/buses");
+      setBusesData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getBusData();
+  }, []);
+  console.log(busesData);
+
+  return (
+    <div>
+      <BusList items={busesData} />
+    </div>
+  );
 };
 
 export default Home;
